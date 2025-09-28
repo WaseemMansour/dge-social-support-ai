aa3e646a3c968c120af8903a088c9877ce6ae4cb
Waseem Mansour <waseem.mn2002@gmail.com>
2025-09-28 13:44:48 +0400

    Link Logo to Home Page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button"
+import { Link } from '@tanstack/react-router'
 import { useTranslation } from 'react-i18next'
+import { PageIdentifier, ROUTES } from '../types/navigation'
 import { LanguageSwitcher } from "./LanguageSwitcher"
 
 interface HeaderProps {
-  currentPage?: "landing" | "wizard"
+  currentPage?: typeof PageIdentifier[keyof typeof PageIdentifier]
 }
 
-export function Header({ currentPage = "landing" }: HeaderProps) {
+export function Header({ currentPage = PageIdentifier.HOME }: HeaderProps) {
   const { t } = useTranslation()
   return (
     <header className="w-full border-b border-gray-300 bg-[#C2B89C]">
@@ -14,20 +16,24 @@ export function Header({ currentPage = "landing" }: HeaderProps) {
         <div className="flex items-center justify-between">
           {/* Logo */}
           <div className="flex items-center">
+          <Link to={ROUTES.HOME}>
             <img 
               src="/dge-abu-dhabi.svg" 
               alt="Department of Government Enablement" 
               className="h-20 w-auto"
             />
+            </Link>
           </div>
 
           {/* User Actions */}
           <div className="flex items-center space-x-3">
             <LanguageSwitcher />
-            {currentPage === "landing" ? (
-              <Button className="bg-gray-800 hover:bg-gray-900 text-white">
-                {t('header.actions.startApplication')}
-              </Button>
+            {currentPage === "home" ? (
+              <Link to={ROUTES.FINANCIAL_ASSISTANCE}>
+                <Button className="bg-gray-800 hover:bg-gray-900 text-white">
+                  {t('header.actions.startApplication')}
+                </Button>
+              </Link>
             ) : null}
           </div>
         </div>
