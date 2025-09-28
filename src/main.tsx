56f146224296da350f08f465611092cf03a39aea
Waseem Mansour <waseem.mn2002@gmail.com>
2025-09-29 00:09:56 +0400

    Add Store Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
+import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { Provider } from 'react-redux'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
+import { store } from './store'
 
-import './styles.css'
 import reportWebVitals from './reportWebVitals.ts'
+import './styles.css'
 
 // Create a new router instance
 const router = createRouter({
@@ -31,7 +33,9 @@ if (rootElement && !rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </StrictMode>,
   )
 }
